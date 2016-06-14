49ef9cdf4864631d3d00a2b2d77267967c375500
Ariel Rey <arielfr@gmail.com>
2016-06-14 12:12:47 -0300

    Adding recursive folder scan. Adding delete all

--- app.js
+++ app.js
@@ -11,79 +11,84 @@ program.version(pkg.version)
 
 program
     .command('put-mappings <file/folder>')
-    .description('Put mappings on elasticsearch index specified on file')
+    .description('Put mappings on elasticsearch index specified on file [-h -p -f -r]')
     .option('-h, --host [host]', 'specified host', 'localhost')
     .option('-p --port [port]', 'specified port', '9200')
     .option('-f --force [force]', 'force the index creation', false)
+    .option('-r --recursive', 'scan directory recursive')
     .action(function(fileDirectory, commander){
         CLI = new CLI({
             host: commander.host,
             port: commander.port
         });
 
-        CLI.putMappings(fileDirectory, { force: commander.force });
+        CLI.putMappings(fileDirectory, { force: commander.force, recursive: ((commander.recursive) ? true : false) ? true : false });
     });
 
 program
     .command('auto-mappings <file/folder>')
-    .description('Put mappings on elasticsearch with auto detect changes')
+    .description('Put mappings on elasticsearch with auto detect changes [-h -p -f -r]')
     .option('-h, --host [host]', 'specified host', 'localhost')
     .option('-p --port [port]', 'specified port', '9200')
     .option('-f --force [force]', 'force the index creation', false)
+    .option('-r --recursive', 'scan directory recursive', false)
     .action(function(fileDirectory, commander){
         CLI = new CLI({
             host: commander.host,
             port: commander.port
         });
 
-        CLI.autoMappings(fileDirectory, { force: commander.force });
+        CLI.autoMappings(fileDirectory, { force: commander.force, recursive: (commander.recursive) ? true : false });
     });
 
 program
     .command('put-index <file/folder>')
-    .description('Put indexes on elasticsearch')
+    .description('Put indexes on elasticsearch [-h -p -r]')
     .option('-h, --host [host]', 'specified host', 'localhost')
     .option('-p --port [port]', 'specified port', '9200')
+    .option('-r --recursive', 'scan directory recursive', false)
     .action(function(fileDirectory, commander){
         CLI = new CLI({
             host: commander.host,
             port: commander.port
         });
 
-        CLI.putIndex(fileDirectory, {});
+        CLI.putIndex(fileDirectory, { recursive: (commander.recursive) ? true : false });
     });
 
 program
     .command('put-data <file/folder>')
-    .description('Put data into a elastic type')
+    .description('Put data into a elastic type [-h -p -r]')
     .option('-h, --host [host]', 'specified host', 'localhost')
     .option('-p --port [port]', 'specified port', '9200')
+    .option('-r --recursive', 'scan directory recursive', false)
     .action(function(fileDirectory, commander){
         CLI = new CLI({
             host: commander.host,
             port: commander.port
         });
 
-        CLI.putData(fileDirectory, {});
+        CLI.putData(fileDirectory, { recursive: (commander.recursive) ? true : false });
     });
 
 program
     .command('put-template <file/folder>')
-    .description('Create elasticsearch templates')
+    .description('Create elasticsearch templates [-h -p -r]')
     .option('-h, --host [host]', 'specified host', 'localhost')
     .option('-p --port [port]', 'specified port', '9200')
+    .option('-r --recursive', 'scan directory recursive', false)
     .action(function(fileDirectory, commander){
         CLI = new CLI({
             host: commander.host,
             port: commander.port
         });
 
-        CLI.putTemplate(fileDirectory, {});
+        CLI.putTemplate(fileDirectory, { recursive: (commander.recursive) ? true : false });
     });
 
 program
     .command('delete-template [name]')
-    .description('delete specific template')
+    .description('delete specific template [-h -p]')
     .option('-h, --host [host]', 'specified host', 'localhost')
     .option('-p --port [port]', 'specified port', '9200')
     .action(function(template, commander){
@@ -97,7 +102,7 @@ program
 
 program
     .command('delete-index [name]')
-    .description('delete specific index')
+    .description('delete specific index [-h -p]')
     .option('-h, --host [host]', 'specified host', 'localhost')
     .option('-p --port [port]', 'specified port', '9200')
     .action(function(index, commander){
@@ -111,7 +116,7 @@ program
 
 program
     .command('delete-by-id [index] [type] [id]')
-    .description('delete specific type element by id')
+    .description('delete specific type element by id [-h -p]')
     .option('-h, --host [host]', 'specified host', 'localhost')
     .option('-p --port [port]', 'specified port', '9200')
     .action(function(index, type, id, commander){
@@ -125,6 +130,25 @@ program
         }
 
         CLI.deleteById(index, type, id);
+    });
+
+program
+    .command('delete-all [index] [type]')
+    .description('delete all the data in an specific type [-h -p]')
+    .option('-h, --host [host]', 'specified host', 'localhost')
+    .option('-p --port [port]', 'specified port', '9200')
+    .action(function(index, type, commander){
+        CLI = new CLI({
+            host: commander.host,
+            port: commander.port
+        });
+
+        if(index === undefined || type === undefined){
+            console.log('All the parameters must be input'.red);
+            return;
+        }
+
+        CLI.deleteAll(index, type);
     });
 
 if (!process.argv.slice(2).length) {
